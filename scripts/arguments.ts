9056b22cf25ed43f5b7abd7b02dd9529781639e7
Dan Rusnac <rusnac14@gmail.com>
2023-11-18 02:48:53 +0300

    feat: updated moderators list

--- scripts/arguments.ts
+++ scripts/arguments.ts
@@ -1,4 +1,8 @@
-export const moderators = ["0xe851A645bd52e16D9d0928e0E71e2BBBeb5aFa9E"]
+export const moderators = [
+    "0x0E1DbabfeB875E76C3a365174bcc06ae7286Be31",
+    "0xE5FEfcB230364ef7f9B5B0df6DA81B227726612b",
+    "0x52eB606f94091A2BA4f985D98C62880a9BA591B6",
+]
 
 export default [
     moderators
