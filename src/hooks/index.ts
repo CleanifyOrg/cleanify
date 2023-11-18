73634393c26555ee5de8de223e622c2033356d7d
Darren Kelly <darren.kelly@vechain.org>
2023-11-18 11:51:20 +0300

    fix useReports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,7 +3,3 @@ export * from "./useTrashifyContract";
 export * from "./useTrashifyReports";
 export * from "./useReportMetadata";
 export * from "./useSubmitReport";
-export * from "./useAddReward";
-export * from "./useApproveReport";
-export * from "./useDeleteReport";
-export * from "./useSubscribeToClean";
