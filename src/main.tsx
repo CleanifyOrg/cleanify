c9366ea833dc08c9891cd9d67e41a0c6509d5ad2
Davide Carpini <davide.carpini@ext.illimity.com>
2023-11-18 12:55:31 +0300

    feat: add screens

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Home } from "./App";
-import { CSSReset, ChakraProvider, Container } from "@chakra-ui/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AccountAbstractionProvider } from "./store/accountAbstractionContext.tsx";
+import { CSSReset, ChakraProvider, Container, Flex } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Navbar } from "./components/Navbar.tsx";
+import { Theme } from "./theme.ts";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router.tsx";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AccountAbstractionProvider>
-      <QueryClientProvider client={queryClient}>
-        <ChakraProvider>
-          <CSSReset />
-          <Navbar />
-          <Container
-            alignItems={"center"}
-            justifyContent={"center"}
-            maxW={"6xl"}
-            display={"flex"}
-            flexGrow={1}
-            overflow={"auto"}
-          >
-            <Home />
-          </Container>
-        </ChakraProvider>
-      </QueryClientProvider>
-    </AccountAbstractionProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={Theme}>
+        <CSSReset />
+        <AccountAbstractionProvider>
+          <Flex flexDir={"column"} h={"100vh"}>
+            <Navbar />
+            <Container
+              alignItems={"center"}
+              justifyContent={"center"}
+              maxW={"6xl"}
+              display={"flex"}
+              flexGrow={1}
+              overflow={"auto"}
+            >
+              <RouterProvider router={router} />
+            </Container>
+          </Flex>
+        </AccountAbstractionProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
