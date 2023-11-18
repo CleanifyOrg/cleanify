18d39ac51d398fb40d5ee56c5265baea76def50d
Erik Nucibella <eriknucib@live.it>
2023-11-18 13:02:50 +0300

    feat(AnalyzeImage): correctly integrate chatgpt

--- src/api/chatgpt/chatgpt.ts
+++ src/api/chatgpt/chatgpt.ts
@@ -1,3 +1,4 @@
+import { OpenAIHelper, blobToBase64, downloadBlob } from "@/utils";
 
 export type AnalyzeImageResponse = {
     isWastePresent: boolean;
@@ -20,13 +21,34 @@ const FALLBACK_RESPONSE = {
 export const analyzeImage = async (
     imageBlobUrl: string
 ): Promise<AnalyzeImageResponse> => {
+    const imageBlob = await downloadBlob(imageBlobUrl);
+    const base64Image = await blobToBase64(imageBlob);
+    const prompt = `
+                    I will send you several images depicting places that need cleaning. Please respond using a JSON object without comments and do not add any other descriptions and comments:
+                    {
+                    'isWastePresent': boolean,
+                    'isWastePollution': boolean, // indicate whether the waste poses an environmental problem
+                    'wasteKind': ['plastic', 'glass', 'paper', 'alluminium'],
+                    'wasteDescription': 'string',
+                    'estimatedWeight': 'number', // estimated weight of the waste in kilograms, It has not to be precise
+                    'estimatedCost': 'number' // estimated cleaning cost by a cleaning company in euros
+                    }
+                `;
 
-    return {
-        isWastePresent: true,
-        isWastePollution: true,
-        wasteKind: [],
-        description: "lorem ipsum dolor sit amet",
-        estimatedWeight: "1000",
-        estimatedCost: "100",
+    const openaiHelper = new OpenAIHelper();
+    try {
+        const response = await openaiHelper.askChatGPTAboutImage({
+            base64Image,
+            prompt,
+        });
+        const jsonString = openaiHelper.getResponseJSONString(response);
+        const result =
+            openaiHelper.parseChatGPTJSONString<AnalyzeImageResponse>(jsonString);
+        if (result) {
+            return result;
+        }
+    } catch (e) {
+        console.error("Failing fetching Chat GPT response:", e);
     }
+    return FALLBACK_RESPONSE;
 };
