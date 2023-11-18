f1d14d55dd35090a3195a68fe5f8937a51c5d757
Dan Rusnac <rusnac14@gmail.com>
2023-11-18 18:46:39 +0300

    fix deploy by switching to netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
-import 'dotenv/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +8,4 @@ export default defineConfig({
   define: {
     global: "globalThis",
   },
-  base: process.env.VITE_IS_PROD === "true" ? "/cleanify/" : "/",
 });
