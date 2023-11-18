ad72d4518c33b33d19d2b119cd1cacabec145437
Erik Nucibella <eriknucib@live.it>
2023-11-18 13:02:37 +0300

    feat(utils): index new utils

--- src/hooks/useReportMetadata.ts
+++ src/hooks/useReportMetadata.ts
@@ -1,30 +1,30 @@
 import { RecordMetadata } from "@models";
 import { useCallback, useEffect, useState } from "react";
-import { getFromIPFS } from "@utils/IPFSUtil.ts";
+import { getFromIPFS } from "@/utils";
 
 export const useReportMetadata = (metadataUri: string) => {
-  const [metadata, setMetadata] = useState<RecordMetadata | null>(null);
-  const [images, setImages] = useState<string[]>([]);
+    const [metadata, setMetadata] = useState<RecordMetadata | null>(null);
+    const [images, setImages] = useState<string[]>([]);
 
-  const fetchMetadata = useCallback(async (uri: string) => {
-    const metadata = JSON.parse(await getFromIPFS(uri)) as RecordMetadata;
+    const fetchMetadata = useCallback(async (uri: string) => {
+        const metadata = JSON.parse(await getFromIPFS(uri)) as RecordMetadata;
 
-    const images = await Promise.all(
-      metadata.images.map((image) => getFromIPFS(image))
-    );
+        const images = await Promise.all(
+            metadata.images.map((image) => getFromIPFS(image))
+        );
 
-    setImages(images);
-    setMetadata(metadata);
-  }, []);
+        setImages(images);
+        setMetadata(metadata);
+    }, []);
 
-  useEffect(() => {
-    fetchMetadata(metadataUri).catch((e) => {
-      console.error("Error fetching metadata:", e);
-    });
-  }, [fetchMetadata, metadataUri]);
+    useEffect(() => {
+        fetchMetadata(metadataUri).catch((e) => {
+            console.error("Error fetching metadata:", e);
+        });
+    }, [fetchMetadata, metadataUri]);
 
-  return {
-    metadata,
-    images,
-  };
+    return {
+        metadata,
+        images,
+    };
 };
--- src/hooks/useTrashifyContract.ts
+++ src/hooks/useTrashifyContract.ts
@@ -1,76 +1,76 @@
 import { useAccountAbstraction } from "@store";
 import { useMemo } from "react";
 import { Trashify__factory as TrashifyFactory } from "@/typechain";
-import {BigNumberish, ethers} from "ethers";
-import {uploadToIpfs} from "@utils/IPFSUtil.ts"
+import { BigNumberish, ethers } from "ethers";
+import { uploadToIpfs } from "@/utils"
 
 export const useTrashifyContract = () => {
-  const { web3Provider, chain } = useAccountAbstraction();
+    const { web3Provider, chain } = useAccountAbstraction();
 
-  const providerOrSigner = useMemo(() => {
-    if (web3Provider) {
-      return web3Provider.getSigner();
-    } else {
-      return new ethers.providers.JsonRpcProvider(
-        chain.rpcUrls.default.http[0]
-      );
+    const providerOrSigner = useMemo(() => {
+        if (web3Provider) {
+            return web3Provider.getSigner();
+        } else {
+            return new ethers.providers.JsonRpcProvider(
+                chain.rpcUrls.default.http[0]
+            );
+        }
+    }, [web3Provider, chain]);
+
+    const contract = useMemo(() => {
+        return TrashifyFactory.connect(chain.contractAddress, providerOrSigner);
+    }, [chain.contractAddress, providerOrSigner]);
+
+
+    const distributeRewards = async (reportId: string) => {
+        const tx = await contract.distributeRewards(reportId)
+        return await tx.wait();
+    };
+
+    const addReward = async (id: number, value: BigNumberish) => {
+        const tx = await contract.addRewards(id, { value });
+
+        return await tx.wait(2);
+    };
+
+    const approveReport = async (id: number) => {
+        const tx = await contract.approveReport(id);
+
+        return await tx.wait(2);
+    };
+
+    const deleteReport = async (reportId: string) => {
+        const tx = await contract.deleteReport(reportId);
+        return await tx.wait();
+    };
+
+    const subscribeToClean = async (reportId: string) => {
+        const tx = await contract.subscribeToClean(reportId);
+        return await tx.wait();
+    };
+
+    const verifyReportCleaning = async (reportId: string, isClean: boolean) => {
+        const tx = await contract.handleVerificationRequest(reportId, isClean)
+        return await tx.wait();
+    };
+
+    const submitCleanedReport = async (reportId: string, imageBase64: string) => {
+        const uri = await uploadToIpfs(imageBase64)
+
+        const tx = await contract.setReportAsCleaned(reportId, uri)
+
+        return await tx.wait();
     }
-  }, [web3Provider, chain]);
 
-  const contract = useMemo(() => {
-    return TrashifyFactory.connect(chain.contractAddress, providerOrSigner);
-  }, [chain.contractAddress, providerOrSigner]);
-
-
-  const distributeRewards = async (reportId: string) => {
-    const tx = await contract.distributeRewards(reportId)
-    return await tx.wait();
-  };
-
-  const addReward = async (id: number, value: BigNumberish) => {
-    const tx = await contract.addRewards(id, { value });
-
-    return await tx.wait(2);
-  };
-
-  const approveReport = async (id: number) => {
-    const tx = await contract.approveReport(id);
-
-    return await tx.wait(2);
-  };
-
-  const deleteReport = async (reportId: string) => {
-    const tx = await contract.deleteReport(reportId);
-    return await tx.wait();
-  };
-
-  const subscribeToClean = async (reportId: string) => {
-    const tx = await contract.subscribeToClean(reportId);
-    return await tx.wait();
-  };
-
-  const verifyReportCleaning = async (reportId: string, isClean: boolean) => {
-    const tx = await contract.handleVerificationRequest(reportId, isClean)
-    return await tx.wait();
-  };
-
-  const submitCleanedReport = async (reportId: string, imageBase64: string) => {
-    const uri = await uploadToIpfs(imageBase64)
-
-    const tx = await contract.setReportAsCleaned(reportId, uri)
-
-    return await tx.wait();
-  }
-
-  return {
-    contract,
-    providerOrSigner,
-    distributeRewards,
-    addReward,
-    approveReport,
-    deleteReport,
-    subscribeToClean,
-    verifyReportCleaning,
-    submitCleanedReport
-  };
+    return {
+        contract,
+        providerOrSigner,
+        distributeRewards,
+        addReward,
+        approveReport,
+        deleteReport,
+        subscribeToClean,
+        verifyReportCleaning,
+        submitCleanedReport
+    };
 };
