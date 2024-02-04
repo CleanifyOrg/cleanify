c383ee5567dda49e297c4c4ce12a04a050510cb2
Erik Nucibella <eriknucib@live.it>
2024-02-04 19:54:19 +0200

    Revert "feat: add dapp-kit"
    This reverts commit da13cedf8af142d9417d3dac9dde080c8e5f5e56.

--- apps/web/src/components/MobileDrawerMenu.tsx
+++ apps/web/src/components/MobileDrawerMenu.tsx
@@ -1,61 +1,115 @@
 import {
-    Button,
-    Drawer,
-    DrawerBody,
-    DrawerCloseButton,
-    DrawerContent,
-    DrawerFooter,
-    DrawerOverlay,
-    VStack,
-    useColorMode,
-    useColorModeValue,
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  HStack,
+  Image,
+  Text,
+  VStack,
+  useColorMode,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import { BsMoon, BsSun } from "react-icons/bs";
-import { WalletButton } from "@vechain/dapp-kit-react";
+import safeLogoDark from "src/assets/safe-info-logo-dark.svg";
+import safeLogo from "src/assets/safe-info-logo-light.svg";
+import { NetworkSelector } from "./NetworkSelector";
+import { useAccountAbstraction } from "@/store";
+import { AddressLabel } from "./AddressLabel";
+
 
 type Props = {
-    isOpen: boolean;
-    onClose: () => void;
+  isOpen: boolean;
+  onClose: () => void;
+  openLoggedAccountModal: () => void;
 };
-export const MobileDrawerMenu = ({ isOpen, onClose }: Props) => {
-    const colorMode = useColorModeValue(
-        { label: "Dark", icon: <BsMoon /> },
-        { label: "Light", icon: <BsSun /> }
-    );
+export const MobileDrawerMenu: React.FC<Props> = ({
+  isOpen,
+  onClose,
+  openLoggedAccountModal,
+}) => {
+  const {
+    chainId,
+    setChainId,
+    isAuthenticated,
+    loginWeb3Auth,
+    ownerAddress,
+    ownerLoading,
+  } = useAccountAbstraction();
 
-    const { toggleColorMode } = useColorMode();
+  const safeLogSrc = useColorModeValue(safeLogo, safeLogoDark);
 
-    return (
-        <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
-            <DrawerOverlay />
-            <DrawerContent>
-                <DrawerCloseButton />
+  const colorMode = useColorModeValue(
+    { label: "Dark", icon: <BsMoon /> },
+    { label: "Light", icon: <BsSun /> }
+  );
 
-                <DrawerBody>
-                    <VStack
-                        spacing={4}
-                        justify="stretch"
-                        align="stretch"
-                        w="full"
-                    >
-                        <WalletButton />
-                    </VStack>
-                </DrawerBody>
+  const { toggleColorMode } = useColorMode();
 
-                <DrawerFooter>
-                    <Button
-                        w="full"
-                        aria-label="Mode Change"
-                        variant="outline"
-                        colorScheme="black"
-                        size="lg"
-                        leftIcon={colorMode.icon}
-                        onClick={toggleColorMode}
-                    >
-                        {colorMode.label}
+  return (
+    <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton />
+        <DrawerHeader>
+          <Box w="70%">
+            <NetworkSelector
+              selectedChainId={chainId}
+              setSelectedChainId={setChainId}
+            />
+          </Box>
+        </DrawerHeader>
+
+        <DrawerBody>
+          <VStack spacing={4} justify="stretch" align="stretch" w="full">
+            <Box>
+              {!isAuthenticated ? (
+                <Button
+                  size="md"
+                  onClick={loginWeb3Auth}
+                  isLoading={ownerLoading}
+                  w="full"
+                >
+                  <HStack spacing={1} alignItems="center">
+                    <Image src={safeLogSrc} alt="Safe logo" boxSize={5} />
+                    <Text>Login</Text>
+                  </HStack>
+                </Button>
+              ) : (
+                <>
+                  {ownerAddress && (
+                    <Button size="md" onClick={openLoggedAccountModal} w="full">
+                      <AddressLabel
+                        address={ownerAddress}
+                        showCopyIntoClipboardButton={false}
+                      />
                     </Button>
-                </DrawerFooter>
-            </DrawerContent>
-        </Drawer>
-    );
+                  )}
+                </>
+              )}
+            </Box>
+          </VStack>
+        </DrawerBody>
+
+        <DrawerFooter>
+          <Button
+            w="full"
+            aria-label="Mode Change"
+            variant="outline"
+            colorScheme="black"
+            size="lg"
+            leftIcon={colorMode.icon}
+            onClick={toggleColorMode}
+          >
+            {colorMode.label}
+          </Button>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
 };
